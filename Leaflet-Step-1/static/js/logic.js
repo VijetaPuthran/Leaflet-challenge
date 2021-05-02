b5992b3f359d67708ca699fed30e0b2a52ec8bf4
VP <vijeta.puthran@gmail.com>
2021-05-02 00:47:33 -0500

    level1 file updated

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -18,13 +18,7 @@ usgs_url ="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.g
 var earthquakes = new L.LayerGroup();
 
 
-// function markerSize(magnitude){
-//     return magnitude * 5;
-// };
-// // combining multiple layers into a group
-// var allData = new L.LayerGroup();
-
-d3.json(usgs_url, function(data){
+d3.json(usgs_url).then(function(data){
     //Create function for color scale based on magnitude
   function color(magnitude){
     // console.log(magnitude)
@@ -54,14 +48,16 @@ d3.json(usgs_url, function(data){
           fillColor: color(feature.properties.mag),
           color: "black",
           weight: 0.1,
-          stroke: true,
-          radius: radius(feature.properties.mag)
+          stroke: true
+          
       };
   }
       // add GeoJSON layer to the map
     L.geoJson(data, {
     pointToLayer: function(feature, latlng) {
-      return L.circleMarker(latlng);
+      return L.circleMarker(latlng,{
+          radius: radius(feature.properties.mag)
+      });
     },
     style: style,
     onEachFeature: function(feature, layer) {
@@ -70,37 +66,8 @@ d3.json(usgs_url, function(data){
 
   }).addTo(earthquakes);
 
-  earthquakes.addTo(map);
+  earthquakes.addTo(myMap);
 
 
 });
 
-
-
-
-//     console.log(data);
-// });
-//     L.geoJSON(data.features, {
-//         // convert point feature to map layer 
-//         pointToLayer: function(feature, coord){
-//             return L.circleMarker(coord, {
-//                 radius: markerSize(feature.properties.mag)
-//             });
-//         },
-//         style: function(dataFeature){
-//             return {
-//                 fillColor: color(dataFeature.properties.mag),
-//                 fillOpacity: 0.5,
-//                 weight: 0.1,
-//                 color: 'black'
-//             }
-//         },
-
-//         onEachFeature: function(feature, layer){
-//             layer.bindPopup('<h3>' + feature.properties.place + '<h3><hr><p>' +
-//             new Date(feature.properties.time) + '</p>');
-//         }
-
-//     }).addTo(allData);
-//     dataMap(allData);
-// });
