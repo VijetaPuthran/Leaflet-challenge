4b3e44e45d88f9e273f67e4a895debc9e1af3c38
VP <vijeta.puthran@gmail.com>
2021-05-01 23:40:32 -0500

    files updated

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,106 @@
+// Create a map object
+var myMap = L.map("mapid", {
+    center: [15.5994, -28.6731],
+    zoom: 3
+  });
+
+  // Create the tile layer that will be the background of our map
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+
+usgs_url ="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+var earthquakes = new L.LayerGroup();
+
+
+// function markerSize(magnitude){
+//     return magnitude * 5;
+// };
+// // combining multiple layers into a group
+// var allData = new L.LayerGroup();
+
+d3.json(usgs_url, function(data){
+    //Create function for color scale based on magnitude
+  function color(magnitude){
+    // console.log(magnitude)
+    if (magnitude > 5){
+        return 'red'
+    } else if (magnitude > 4){
+        return 'orange'
+    } else if (magnitude > 3){
+        return 'yellow'
+    } else if (magnitude > 2){
+        return 'green'
+    } else if (magnitude > 1){
+        return 'blue'
+    } else {
+        return 'purple'
+    }
+  }
+    //Create function for radius of the circle based on the magnitude
+  function radius(magnitude){
+      return magnitude * 5;
+    }
+    //Create function for style info
+  function style(feature){
+      return{
+          opacity: 1,
+          fillOpacity: 0.5,
+          fillColor: color(feature.properties.mag),
+          color: "black",
+          weight: 0.1,
+          stroke: true,
+          radius: radius(feature.properties.mag)
+      };
+  }
+      // add GeoJSON layer to the map
+    L.geoJson(data, {
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+    style: style,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+
+  }).addTo(earthquakes);
+
+  earthquakes.addTo(map);
+
+
+});
+
+
+
+
+//     console.log(data);
+// });
+//     L.geoJSON(data.features, {
+//         // convert point feature to map layer 
+//         pointToLayer: function(feature, coord){
+//             return L.circleMarker(coord, {
+//                 radius: markerSize(feature.properties.mag)
+//             });
+//         },
+//         style: function(dataFeature){
+//             return {
+//                 fillColor: color(dataFeature.properties.mag),
+//                 fillOpacity: 0.5,
+//                 weight: 0.1,
+//                 color: 'black'
+//             }
+//         },
+
+//         onEachFeature: function(feature, layer){
+//             layer.bindPopup('<h3>' + feature.properties.place + '<h3><hr><p>' +
+//             new Date(feature.properties.time) + '</p>');
+//         }
+
+//     }).addTo(allData);
+//     dataMap(allData);
+// });
