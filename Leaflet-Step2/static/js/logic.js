6eebaec28340c3a8bcbffdb256b0a0b8cbe543a4
VP <vijeta.puthran@gmail.com>
2021-05-02 21:19:53 -0500

    legend updated

--- Leaflet-Step2/static/js/logic.js
+++ Leaflet-Step2/static/js/logic.js
@@ -8,6 +8,7 @@ var usgs_url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_mo
 
 //Variable to hold the layers into a group
     var group = new L.LayerGroup();
+
 //Using promise with the usage of 'then' method to extract data
     d3.json(usgs_url).then(function(data){
         L.geoJSON(data.features, {
@@ -34,10 +35,13 @@ var usgs_url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_mo
         }).addTo(group);
         dataMap(group); 
     });
+
 //Link to get the data on tectonic plates
 var tect_plate = 'https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json';
+
 //Creating a new layer for the boundary
 var boundary = new L.LayerGroup();
+
 //Using promise with the usage of 'then' method to extract data
 d3.json(tect_plate).then(function(data){
     //console.log(data)
@@ -50,6 +54,7 @@ d3.json(tect_plate).then(function(data){
         },
     }).addTo(boundary);
 })
+
 //Creating function for color scale based on magnitude
 function color(magnitude){
     if (magnitude > 5){
@@ -66,6 +71,7 @@ function color(magnitude){
         return 'purple'
     }
 };
+
 //Creating the function for map with base and other layers
 function dataMap(){
     var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -109,4 +115,24 @@ function dataMap(){
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(myMap);
+
+//Setting up the legend
+    var legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function(myMap){
+        var div = L.DomUtil.create("div", "info legend"),
+         magnitude = [0, 1, 2, 3, 4, 5];
+         colors = [];
+//Adding the range for colors and magnitude
+    div.innerHTML += "<h4 style='margin:4px'>Range</h4>"
+
+    for (var i = 0; i < magnitude.length; i++) {
+        div.innerHTML += "<i style='background: " + color(magnitude[i] +1) + "'></i> " +
+          magnitude[i] + ( magnitude[i + 1] ? "&ndash;" + magnitude[i + 1] + "<br>" : "+");
+      }
+      return div;
+    };
+
+//Adding legend to the map    
+    legend.addTo(myMap); 
 }
