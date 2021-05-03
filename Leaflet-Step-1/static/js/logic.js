be14f604234344399e9c8e4fafba641b839c30c0
VP <vijeta.puthran@gmail.com>
2021-05-02 19:19:13 -0500

    final js file update

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,11 +1,11 @@
-// Create a map object
+// Creating a map object
 var myMap = L.map("mapid", {
     center: [15.5994, -28.6731],
     zoom: 4
   });
 
-  // Create the tile layer that will be the background of our map
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+// Creating the tile layer that will be the background of our map
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
@@ -14,12 +14,16 @@ var myMap = L.map("mapid", {
     accessToken: API_KEY
   }).addTo(myMap);
 
+//Link used to grab the monthly earthquake data from usgs.
 usgs_url ="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+//Adding a new layer to show the earthquakes in terms of magnitude.
 var earthquakes = new L.LayerGroup();
 
-
+//Using promise with the usage of 'then' method to extract data
 d3.json(usgs_url).then(function(data){
-    //Create function for color scale based on magnitude
+    //console.log(data)
+    
+//Creating function for color scale based on magnitude
   function color(magnitude){
     // console.log(magnitude)
     if (magnitude > 5){
@@ -36,11 +40,11 @@ d3.json(usgs_url).then(function(data){
         return 'purple'
     }
   }
-    //Create function for radius of the circle based on the magnitude
+//Creating function for radius of the circle based on the magnitude
   function radius(magnitude){
       return magnitude * 5;
     }
-    //Create function for style info
+//Creating function for style info
   function style(feature){
       return{
           opacity: 1,
@@ -52,7 +56,7 @@ d3.json(usgs_url).then(function(data){
           
       };
   }
-      // add GeoJSON layer to the map
+// Adding GeoJSON layer to the map
     L.geoJson(data, {
     pointToLayer: function(feature, latlng) {
       return L.circleMarker(latlng,{
@@ -65,10 +69,10 @@ d3.json(usgs_url).then(function(data){
     }
 
   }).addTo(earthquakes);
+//Adding the earthquake layer to the map
+earthquakes.addTo(myMap);
 
-  earthquakes.addTo(myMap);
-
-  // Set up the legend
+//Setting up the legend
   var legend = L.control({ position: "bottomright" });
 
   legend.onAdd = function() {
@@ -76,7 +80,7 @@ d3.json(usgs_url).then(function(data){
     magnitude = [0, 1, 2, 3, 4, 5];
     colors = [];
 
-    // Adding the range for colors and magnitude
+//Adding the range for colors and magnitude
     div.innerHTML += "<h4 style='margin:4px'>Range</h4>"
 
     for (var i = 0; i < magnitude.length; i++) {
@@ -86,7 +90,7 @@ d3.json(usgs_url).then(function(data){
       return div;
   };
 
-  // Adding legend to the map
+//Adding legend to the map
   legend.addTo(myMap);
 
 });
